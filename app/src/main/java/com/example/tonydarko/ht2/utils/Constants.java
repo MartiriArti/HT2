f3d174d9505cf1efa4550391fe72cb46c1031200
Tony Darko <antonyo007h@gmail.com>
2018-01-22 15:41:25 +0200

    ///

---
+++ app/src/main/java/com/example/tonydarko/ht2/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.example.tonydarko.ht2.utils;
+
+public class Constants {
+    public static final int NUMBER = 50;
+    public static final int SLEEP_TIME = 500;
+
+    public static final int COUNT_BUYS = 2;
+    public static final int MAXIMUM_PURCHASE = 10;
+    public static final int NUMBER_OF_TYPES = 5;
+
+
+}
